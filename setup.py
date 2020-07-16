472715ceeffbf69a95643b19e94c8a62c911b168
quantrading <dnwogo@naver.com>
2020-07-16 13:14:37 +0900

    0.1.15

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="quantrading",
-    version='0.1.14',
+    version='0.1.15',
     url="https://github.com/quantrading/quantrading",
     license="MIT",
     author="Jang Woo Jae",
