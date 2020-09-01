036530d279f8a89ec1cb05311c620a9cd5cad1c5
quantrading <dnwogo@naver.com>
2020-09-01 11:41:00 +0900

    0.1.23   - bug fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="quantrading",
-    version='0.1.22',
+    version='0.1.23',
     url="https://github.com/quantrading/quantrading",
     license="MIT",
     author="Jang Woo Jae",
